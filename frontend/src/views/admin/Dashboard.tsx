74b2de09e1617b67198a1b1bef553321b9868d02
mannguyen <nguyentrongminhman95@gmail.com>
2023-01-08 14:42:16 +0700

    update

--- frontend/src/views/admin/Dashboard.tsx
+++ frontend/src/views/admin/Dashboard.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
 import Layout from "../../components/backend/Layout";
+import { Card, CardHeader, CardContent, Typography } from "@mui/material";
 interface DashBoardProps {}
 
 const DashBoard: React.FunctionComponent<DashBoardProps> = () => {
-  return <Layout>dssadsdsddsa</Layout>;
+  return (
+    <Layout>
+      <Card>
+        <CardHeader title="DashBoard" />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+      </Card>
+    </Layout>
+  );
 };
 
 export default DashBoard;
